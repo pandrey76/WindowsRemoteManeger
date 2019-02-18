e85dba894edb31f15e6a5cab465384dc1969120e
pandrey76 <pandrey76@yandex.ru>
2019-02-18 14:21:22 +0300

    Send and read email from gmail.com!
    Make base work logic for sending and reading email from Gmail.com.

---
+++ Mailing/Sending_to_Gmail.py
@@ -0,0 +1,31 @@
+
+import smtplib
+from email.mime.text import MIMEText
+# from datetime import datetime
+from GMAIL_PWD import GMAIL_PWD, MAIN_EMAIL, FROM_WHO
+
+
+def send_gmail(msg_str):
+    msg = MIMEText(msg_str, 'plain')
+
+    msg['Subject'] = FROM_WHO
+    msg['From'] = MAIN_EMAIL
+    msg['To'] = MAIN_EMAIL
+
+    server = smtplib.SMTP('smtp.gmail.com', port=587)
+    server.ehlo()  # Extended Hello
+    server.starttls()  # Put the SMTP connection in TLS (Transport Layer Security) mode.
+    server.ehlo()  # All SMTP commands that follow will be encrypted.
+    # server.login('pintchukandrey76@gmail.com', GMAIL_PWD)
+    server.login(MAIN_EMAIL, GMAIL_PWD)
+    msg["Received:"] = "from Windows 10 Pro ([200.200.200.200])"
+    server.send_message(msg)
+
+    # server.mail("pinchukandreyurevich76@gmail.com", messages)
+    server.close()
+
+
+# def take_email()
+# ===========================================
+if __name__ == '__main__':
+    send_gmail("01 minutes")
