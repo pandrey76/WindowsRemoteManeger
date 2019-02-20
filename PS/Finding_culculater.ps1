906c3276e36331cd1f624a8e19b30e9b4d3b32cb
pandrey76 <pandrey76@yandex.ru>
2019-02-20 18:07:15 +0300

    Getting PackageId for "calculator" from PS for Current User!

--- PS/Finding_culculater.ps1
+++ PS/Finding_culculater.ps1
@@ -2,11 +2,11 @@
 Function GetPackageFamilyName([String]$patern, [String]$user)
 {
 
-    $apx_package = Get-AppxPackage  # -User $user
+    $apx_package = Get-AppxPackage -User $user
     foreach ($apx_pack in $apx_package)
     {
         $temp_str = $apx_pack.PackageFamilyName
-        if ($temp_str.Contains($patern))#alculator
+        if ($temp_str.Contains($patern))
         {
             Return ($temp_str)
         }
