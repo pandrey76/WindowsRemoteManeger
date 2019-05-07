a8cc6b7ae726e7ce0812465b5f650d64f6ae11b4
pandrey76 <pandrey76@yandex.ru>
2019-05-07 16:53:25 +0300

    Forget adding some files to project!
    Refactoring DB tables, inject testing, refactoring old files of project.

---
+++ Common/User.py
@@ -0,0 +1,213 @@
+import sqlite3
+
+
+class User:
+    """
+
+    """
+
+    def __init__(self, name, blocked_state, online_permission, work_seconds_delay, start_session_time, current_time):
+        """
+
+        :param name:
+        :param blocked_state:
+        :param online_permission:
+        :param work_seconds_delay:
+        :param start_session_time:
+        :param current_time:
+        """
+        self.__Name = name
+        if isinstance(name, str) is False:
+            raise TypeError()
+
+        self.__BlockedState = blocked_state
+        self.__OnlinePermission = online_permission
+        self.__WorkSecondsDelay = work_seconds_delay
+        self.__StartSessionTime = start_session_time
+        self.__CurrentTime = current_time
+
+    name = property(lambda self: self.__Name)
+    """
+    """
+
+    @property
+    def blocked_state(self):
+        """
+
+        :return:
+        """
+        return self.__BlockedState
+
+    @blocked_state.setter
+    def blocked_state(self, param):
+        """
+
+        :param param:
+        :return:
+        """
+        self.__BlockedState = param
+
+    @property
+    def online_permission(self):
+        """
+
+        :return:
+        """
+        return self.__OnlinePermission
+
+    @online_permission.setter
+    def online_permission(self, param):
+        """
+
+        :param param:
+        :return:
+        """
+        self.__OnlinePermission = param
+
+    @property
+    def work_seconds_delay(self):
+        """
+
+        :return:
+        """
+        return self.__WorkSecondsDelay
+
+    @work_seconds_delay.setter
+    def work_seconds_delay(self, param):
+        """
+
+        :param param:
+        :return:
+        """
+        self.__WorkSecondsDelay = param
+
+    @property
+    def start_session_time(self):
+        """
+
+        :return:
+        """
+        return self.__StartSessionTime
+
+    @start_session_time.setter
+    def start_session_time(self, param):
+        """
+
+        :param param:
+        :return:
+        """
+        self.__StartSessionTime = param
+
+    @property
+    def current_time(self):
+        """
+
+        :return:
+        """
+        return self.__CurrentTime
+
+    @current_time.setter
+    def current_time(self, param):
+        """
+
+        :param param:
+        :return:
+        """
+        self.__CurrentTime = param
+
+    @staticmethod
+    def create_user(db_instance, user):
+        """
+
+        :param db_instance:
+        :param user:
+        :return:
+        """
+        try:
+            db_instance.cursor.execute("""
+                                        CREATE TABLE IF NOT EXISTS 
+                                        users (
+                                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                             user_name TEXT,
+                                             isBlocked INTEGER, 
+                                             workOnline INTEGER,
+                                             sessionSecondsDelay INTEGER,
+                                             startTime REAL,
+                                             currentTime REAL
+                                             )
+                                  """)
+            db_instance.connection.commit()
+        except sqlite3.Error as ex:
+            pass  # print(ex)
+        db_instance.cursor.execute("""
+                                        SELECT count(*) FROM 
+                                        users 
+                                        WHERE user_name = ?""",
+                                   (user.name,))
+        data = db_instance.cursor.fetchone()[0]
+        if data == 0:
+            try:
+                db_instance.cursor.execute("""INSERT INTO users VALUES(?, ?, ?, ?, ?, ?, ?)""", (
+                                                                                              None,
+                                                                                              user.name,
+                                                                                              user.blocked_state,
+                                                                                              user.online_permission,
+                                                                                              user.work_seconds_delay,
+                                                                                              user.start_session_time,
+                                                                                              user.current_time))
+                db_instance.connection.commit()
+            except sqlite3.Error as ex:
+                pass  # print(ex)
+
+    @staticmethod
+    def get_user(db_instance, user_name):
+        """
+
+        :param user_name:
+        :param db_instance:
+        :return:
+        """
+        try:
+            db_instance.cursor.execute("""SELECT 
+                                                user_name,
+                                                isBlocked,
+                                                workOnline,
+                                                sessionSecondsDelay,
+                                                startTime,
+                                                currentTime
+                                                FROM users WHERE user_name=:UserName""", {"UserName": user_name})
+
+            db_instance.connection.commit()
+            row = db_instance.cursor.fetchone()
+            getting_user = User(row[0], row[1], row[2], row[3], row[4], row[5])
+            return getting_user
+        except sqlite3.Error as ex:
+            pass  # print(ex)
+
+    @staticmethod
+    def update_user(db_instance, user):
+        """
+
+        :param user:
+        :param db_instance:
+        :return:
+        """
+        try:
+            db_instance.cursor.execute("""UPDATE users SET
+                                                isBlocked = ?,
+                                                workOnline = ?,
+                                                sessionSecondsDelay = ?,
+                                                startTime = ?,
+                                                currentTime = ?
+                                                WHERE user_name = ?""", (
+                                                                            user.blocked_state,
+                                                                            user.work_seconds_delay,
+                                                                            user.work_seconds_delay,
+                                                                            user.start_session_time,
+                                                                            user.current_time,
+                                                                            user.name
+                                                                         ))
+
+            db_instance.cursor.commit()
+        except sqlite3.Error as ex:
+            pass  # print(ex)
---
+++ test/TestDBUser.py
@@ -0,0 +1,67 @@
+import unittest
+import os.path
+import importlib.util
+import time
+
+
+class TestDBUser(unittest.TestCase):
+
+    def setUp(self) :
+        """
+
+        """
+        path_to_scripts = os.path.dirname(os.path.realpath(__file__))
+        path_to_scripts = path_to_scripts + os.sep + ".." + os.sep
+        path_to_scripts = os.path.abspath(path_to_scripts)
+        path_to_scripts = os.path.join(path_to_scripts, 'Common')
+        path_to_scripts = os.path.join(path_to_scripts, 'User.py')
+        spec1 = importlib.util.spec_from_file_location("User.User", path_to_scripts)
+        self.__UserModule = importlib.util.module_from_spec(spec1)
+        spec1.loader.exec_module(self.__UserModule)
+        self.__UserName = "Admin1"
+
+        path_to_scripts = os.path.dirname(os.path.realpath(__file__))
+        path_to_scripts = path_to_scripts + os.sep + ".." + os.sep
+        path_to_scripts = os.path.abspath(path_to_scripts)
+        path_to_common_scripts = os.path.join(path_to_scripts, 'Common')
+        path_to_common_scripts = os.path.join(path_to_common_scripts, 'SQLighte_Performance.py')
+        spec2 = importlib.util.spec_from_file_location("SQLighte_Performance.DBPerformance", path_to_common_scripts)
+        db_module = importlib.util.module_from_spec(spec2)
+        spec2.loader.exec_module(db_module)
+        self.__DB = db_module.DBPerformance(os.path.join(os.path.expanduser(os.getenv('USERPROFILE')),
+                                                         "my_sqlite_0.db"))
+
+    def test_create_and_read_user(self):
+        """
+
+        :return:
+        """
+
+        user1 = self.__UserModule.User(self.__UserName,
+                                       True,         # blocked_state
+                                       False,        # online_permission
+                                       7200,         # work_seconds_delay
+                                       time.time(),  # start_session_time
+                                       time.time() + 200     # current_time
+                                       )
+        self.__UserModule.User.create_user(self.__DB, user1)
+
+        self.assertTrue('FOO'.isupper())
+        # self.assertFalse('Foo'.isupper())
+
+    def test_read_user_from_db(self):
+        """
+
+        :return:
+        """
+        user_name = "Admin1"
+        user = self.__UserModule.User.get_user(self.__DB, user_name)
+        self.assertTrue(user.name, user_name)
+        self.assertTrue(user.blocked_state, True)
+        self.assertTrue(user.online_permission, False)
+        self.assertTrue(user.work_seconds_delay, 7200)
+        # self.assertTrue(user.name, user_name)
+
+
+if __name__ == '__main__':
+    unittest.main()
