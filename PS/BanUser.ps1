04b32221f9247aa0fd7d9ff86318c1f739979517
pandrey76 <pandrey76@yandex.ru>
2020-10-27 19:27:14 +0300

    Editing BanUser.ps1 file.
    Remove User Logoff logic.

--- PS/BanUser.ps1
+++ PS/BanUser.ps1
@@ -1,12 +1,17 @@
-./LogoffUser.ps1
+﻿# "./LogoffUser.ps1"
+
+
 function GetPackageFamilyName([String]$patern, [String]$user)
 {
-    $apx_package = Get-AppxPackage -User $user
+    $apx_package = Get-AppxPackage -User $user # | Out-File -FilePath "c:\\apx_package.txt"
+#    Write-Host $apx_package
+
     foreach ($apx_pack in $apx_package)
     {
         $temp_str = $apx_pack.PackageFamilyName
         if ($temp_str.Contains($patern))
         {
+            Write-Host $temp_str
             Return ($temp_str)
         }
     }
@@ -15,9 +20,10 @@ function GetPackageFamilyName([String]$patern, [String]$user)
 
 function BanCurrentUser([String]$user)
 {
+    Write-Host  $user
     $calculater_id = GetPackageFamilyName -patern "alculator" -user $user
     Set-AssignedAccess -AppUserModelId $calculater_id!app -UserName $user
-    Get-Quser-v2 -user $user
+    # Get-Quser-v2 -user $user
 }
 
-BanCurrentUser -user $args[0]
+ BanCurrentUser -user $args[0]
