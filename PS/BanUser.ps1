12ffba758d3d861aa07ebb12cd4a625b9529a59c
pandrey76 <pandrey76@yandex.ru>
2020-10-29 19:29:12 +0300

    Edited BanUser.ps1 file.
    All params sending to script from command line. Now all work.

--- PS/BanUser.ps1
+++ PS/BanUser.ps1
@@ -1,17 +1,16 @@
 ﻿# "./LogoffUser.ps1"
 
-
+################################################################
+# Correcting Work!!!!
+<#
 function GetPackageFamilyName([String]$patern, [String]$user)
 {
-    $apx_package = Get-AppxPackage -User $user # | Out-File -FilePath "c:\\apx_package.txt"
-#    Write-Host $apx_package
-
+    $apx_package = Get-AppxPackage -User $user
     foreach ($apx_pack in $apx_package)
     {
         $temp_str = $apx_pack.PackageFamilyName
         if ($temp_str.Contains($patern))
         {
-            Write-Host $temp_str
             Return ($temp_str)
         }
     }
@@ -20,10 +19,34 @@ function GetPackageFamilyName([String]$patern, [String]$user)
 
 function BanCurrentUser([String]$user)
 {
-    Write-Host  $user
     $calculater_id = GetPackageFamilyName -patern "alculator" -user $user
     Set-AssignedAccess -AppUserModelId $calculater_id!app -UserName $user
     # Get-Quser-v2 -user $user
 }
 
- BanCurrentUser -user $args[0]
+BanCurrentUser -user $args[0]
+#>
+################################################################
+
+function GetPackageFamilyName([String]$AppPattern, [String]$UserName)
+{
+    $apx_package = Get-AppxPackage -User $UserName
+    foreach ($apx_pack in $apx_package)
+    {
+        $app_full_name = $apx_pack.PackageFamilyName
+        if ($app_full_name.ToUpper().Contains($AppPattern.ToUpper()))
+        {
+            Return ($app_full_name)
+        }
+    }
+}
+
+
+function BanCurrentUser([String]$UserName, [String]$AppPattern)
+{
+    $app_family_name = GetPackageFamilyName -AppPattern $AppPattern -user $UserName
+    Set-AssignedAccess -AppUserModelId $app_family_name!app -UserName $UserName
+}
+
+BanCurrentUser -user $args[0] -AppPattern $args[1]
+# C:\Work\WindowsRemoteManeger\PS>powershell "C:\Work\WindowsRemoteManeger\PS\BanUser.ps1" "Ogurchuk" "alculator"
